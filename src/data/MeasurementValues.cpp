cb6dc7f987b8973f156396be3a695fec665d9843
Chone Theo <tchone@2D-LINUX-139.insa-lyon.fr>
2023-05-10 17:19:45 +0200

    compute quality and distance

--- src/data/MeasurementValues.cpp
+++ src/data/MeasurementValues.cpp
@@ -1,11 +1,26 @@
 #include "MeasurementValues.h"
+#include <math.h>
 
 int MeasurementValues::ComputeQualityIndex() const {
-    return 0;
+    static int tabO3[9] = {29,54,79,104,129,149,179,209,239};
+    static int tabSO2[9] = {39,79,119,159,199,249,299,399,499};
+    static int tabNO2[9] = {29,54,84,109,134,164,199,274,399};
+    static int tabPM10[9] = {6,13,20,27,34,41,49,64,79};
+    int indice = 10;
+
+    for(int i=9;i>0;i--){
+        if(this->GetO3() > tabO3[i] || this->GetSO2() > tabSO2[i] || this->GetNO2() > tabNO2[i] || this->GetPM10() > tabPM10[i]){
+            break;
+        }
+        indice--;
+    }
+
+    return indice;
 }
 
-int MeasurementValues::ComputeSimilarity(const MeasurementValues& values) const {
-    return 0;
+double MeasurementValues::ComputeSimilarity(const MeasurementValues& values) const {
+    double value = pow(this->GetO3()-values.GetO3(),2)+pow(this->GetSO2()-values.GetSO2(),2)+pow(this->GetNO2()-values.GetNO2(),2)+pow(this->GetPM10()-values.GetPM10(),2);
+    return value;
 }
 
 MeasurementValues MeasurementValues::operator+(const MeasurementValues& values) const {
